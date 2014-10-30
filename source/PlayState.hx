d62923fc3e371bff47026e5e6f3f23a22c947b65
Guilherme Fernandes <gfernandes@gazeus.com>
2014-10-30 16:44:03 -0200

    - Map generation;

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,6 +6,7 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.tile.FlxTilemap;
+import flixel.math.FlxPoint;
 
 /**
  * A FlxState which can be used for the actual gameplay.
@@ -21,9 +22,30 @@ class PlayState extends FlxState
 		
 		var map:FlxTilemap = new FlxTilemap();
 
-		trace(MapGenerator.diamondSquare(100, 100));
+		var heights:Array<Array<Float>> = MapGenerator.diamondSquare(100, 100);
 		
-		// map.loadMapFrom2DArray(, "assets/images/world.png", 32, 32);
+		var indexes:Array<Array<Int>> = new Array<Array<Int>>();
+		
+		for(i in 0 ... 100)
+		{
+			indexes[i] = new Array<Int>();
+			
+			for(j in 0 ... 100)
+			{
+				if(heights[i][j] > 0.5)
+				{
+					indexes[i][j] = 2;
+				}
+				else
+				{
+					indexes[i][j] = 1;
+				}
+			}
+		}
+		
+		map.loadMapFrom2DArray(indexes, "assets/images/world.png", 32, 32);
+		
+		add(map);
 	}
 	
 	/**
@@ -38,8 +60,29 @@ class PlayState extends FlxState
 	/**
 	 * Function that is called once every frame.
 	 */
+	
+// 	private var touchPoint:FlxPoint;
+	
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-	}	
+
+		// Check Input
+
+		if (FlxG.mouse.justPressed)
+		{
+			touchPoint = FlxG.mouse.getScreenPosition();
+		}
+			
+		if(FlxG.mouse.pressed)
+		{
+			var newPoint:FlxPoint = FlxG.mouse.getScreenPosition();
+			
+			trace(newPoint.subtractPoint(touchPoint));
+		}
+		else if(FlxG.mouse.justReleased)
+		{
+			touchPoint = null;
+		}
+	}
 }

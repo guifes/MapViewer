6ba4ab4b7b806c457291e7630a849bed6a82d863
Guilherme <guifes@gmail.com>
2023-04-25 23:56:43 +0100

    Fixing map scrolling

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,17 +1,13 @@
 package;
 
-import haxe.Resource;
 import haxe.ui.Toolkit;
-import haxe.ui.core.Screen;
-import openfl.Assets;
-import openfl.Lib;
 import openfl.display.Sprite;
 import simple.SPEngine;
 import state.MenuState;
 
 class Main extends Sprite
 {
-	static inline var TARGET_WIDTH = 600;
+	static inline var TARGET_WIDTH = 1024;
 
 	public function new()
 	{
--- Source/state/MenuState.hx
+++ Source/state/MenuState.hx
@@ -1,12 +1,47 @@
 package state;
 
+import openfl.Assets;
+import openfl.display.BitmapData;
+import openfl.geom.Point;
+import simple.SPEngine;
 import simple.display.SPState;
 import simple.display.tilemap.SPTileMap;
 
 class MenuState extends SPState
 {
+	private static inline var TILE_SIZE = 32;
+	private static inline var WIDTH = 64;
+	private static inline var HEIGHT = 64;
+
+    var tilemap: SPTileMap;
+
 	public override function init()
 	{
-		var tilemap = new SPTileMap();
+		var bitmapData: BitmapData = Assets.getBitmapData("assets/textures/atlas.png");
+		
+        tilemap = new SPTileMap();
+        
+        tilemap.loadMapFromArray([for(i in 0...WIDTH * HEIGHT) i], WIDTH, HEIGHT, bitmapData, TILE_SIZE, TILE_SIZE);
+        
+        add(tilemap);
 	}
+
+    var tilemap_pos: Point;
+    var initial_pos: Point;
+
+    public override function update(elapsed: Int, deltaTime: Int)
+    {
+        var pos = SPEngine.mouseManager.worldMouse.get_screen_position();
+
+        if(SPEngine.mouseManager.worldMouse.left.just_pressed)
+        {
+			tilemap_pos = new Point(tilemap.x, tilemap.y);
+			initial_pos = pos;
+        }
+        else if(SPEngine.mouseManager.worldMouse.left.pressed)
+        {
+			tilemap.x = (pos.x - initial_pos.x) + tilemap_pos.x;
+            tilemap.y = (pos.y - initial_pos.y) + tilemap_pos.y;
+        }
+    }
 }
